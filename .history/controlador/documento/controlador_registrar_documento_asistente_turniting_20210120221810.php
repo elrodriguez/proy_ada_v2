0ae39bcaeb3100a761d38109d2a114a9cb4947be
elmer rodriguez <elrodriguez2423@gmail.com>
2021-01-20 22:18:22 -0500

    ddddd

---
+++ .history/controlador/documento/controlador_registrar_documento_asistente_turniting_20210120221810.php
@@ -0,0 +1,29 @@
+<?php
+	$cont=0;
+	$iddocumento = $_POST['iddocumentoturniting'];
+	if ($_FILES["file-turniting"]['tmp_name']!="") {
+    	$imagen=uniqid()."-".$_FILES['file-turniting']['name'];
+		$ruta1=$_FILES['file-turniting']['tmp_name'];
+		
+		$dir='../../Archivo/'.$iddocumento.'/';
+
+		if (!file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
+		
+		$destino1 = $dir.$imagen;
+		$nombre1 = 'Archivo/'.$iddocumento.'/'.$imagen;
+		//print_r($ruta1);exit;
+		copy($ruta1, $destino1);
+		$cont=1;
+	}else{
+		$destinoImagen="";
+	}
+	$porcentaje = $_POST['porcentaje'];
+	
+	require '../../modelo/modelo_documento_asistente.php';
+
+	$MC = new Modelo_documento();
+	$consulta = $MC->subir_documento_turniting($iddocumento,$porcentaje,$destinoImagen);
+	echo $consulta;
+?>
--- controlador/documento/controlador_registrar_documento_asistente_turniting.php
+++ controlador/documento/controlador_registrar_documento_asistente_turniting.php
@@ -1,17 +1,26 @@
 <?php
 	$cont=0;
+	$iddocumento = $_POST['iddocumentoturniting'];
 	if ($_FILES["file-turniting"]['tmp_name']!="") {
     	$imagen=uniqid()."-".$_FILES['file-turniting']['name'];
 		$ruta1=$_FILES['file-turniting']['tmp_name'];
-		$destinoImagen='Archivo/'.$imagen;
+		
+		$dir='../../Archivo/'.$iddocumento.'/';
+
+		if (!file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
+		
+		$destino1 = $dir.$imagen;
+		$nombre1 = 'Archivo/'.$iddocumento.'/'.$imagen;
 		//print_r($ruta1);exit;
-		copy($ruta1, $destinoImagen);
+		copy($ruta1, $destino1);
 		$cont=1;
 	}else{
 		$destinoImagen="";
 	}
 	$porcentaje = $_POST['porcentaje'];
-	$iddocumento = $_POST['iddocumentoturniting'];
+	
 	require '../../modelo/modelo_documento_asistente.php';
 
 	$MC = new Modelo_documento();
