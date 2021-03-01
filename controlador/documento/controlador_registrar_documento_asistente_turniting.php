6e5dab28b3057be25c39fa3e804852bb777a679d
Jhordan07 <jhordanjhonter07@gmail.com>
2021-02-28 21:31:29 -0500

    final

--- controlador/documento/controlador_registrar_documento_asistente_turniting.php
+++ controlador/documento/controlador_registrar_documento_asistente_turniting.php
@@ -4,15 +4,15 @@
 	if ($_FILES["file-turniting"]['tmp_name']!="") {
     	$imagen=uniqid()."-".$_FILES['file-turniting']['name'];
 		$ruta1=$_FILES['file-turniting']['tmp_name'];
-		
+
 		$dir='../../Archivo/'.$iddocumento.'/';
 
 		if (!file_exists($dir)) {
 			mkdir($dir, 0777, true);
 		}
-		
+
 		$destino1 = $dir.$imagen;
-		$nombre1 = 'Archivo/'.$iddocumento.'/'.$imagen;
+		$nombre1 = str_replace(' ','-','Archivo/'.$iddocumento.'/'.$imagen);
 		//print_r($ruta1);exit;
 		copy($ruta1, $destino1);
 		$cont=1;
@@ -20,7 +20,7 @@
 		$destinoImagen="";
 	}
 	$porcentaje = $_POST['porcentaje'];
-	
+
 	require '../../modelo/modelo_documento_asistente.php';
 
 	$MC = new Modelo_documento();
